5e0e0e9faf3393f6ca11081ad41af080e9ade281
elfbars <122835363+elfbars@users.noreply.github.com>
2023-05-28 23:42:09 +0100

    Create fxmanifest.lua

---
+++ dmUser/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {  'gta5' }
+
+-- server scripts
+server_scripts{ 
+  "@vrp/lib/utils.lua",
+  "sv_dm.lua",
+}
+
+-- if you aren't using vRP because you're some cool guy that changed vRP -> FNR then please change the fucking resource to get the lib from you absolute spastics
