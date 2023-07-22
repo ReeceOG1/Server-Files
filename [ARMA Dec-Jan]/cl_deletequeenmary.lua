9ae7e00f0ac0c13a4bb299b528e1d749f342b9be
elfbars <122835363+elfbars@users.noreply.github.com>
2023-07-22 22:54:11 +0100

    Update cl_deletequeenmary.lua

--- [ARMA Dec-Jan]/cl_deletequeenmary.lua
+++ [ARMA Dec-Jan]/cl_deletequeenmary.lua
@@ -2,7 +2,6 @@ hey guys this is how to delete big boats at rig
 
 @TomDevCMG please use this when Kimber imports about 20 of em thanks
 
-```lua
 local function delQM2()
     local f = tARMA.getPlayerVehicle()
 	local be = GetEntityModel(f)
@@ -11,4 +10,3 @@ local function delQM2()
     end
 end
 tARMA.createArea("rig_delete_qm2",vector3(-1703.7, 8886.5, 28.7),300.0,250.0,delQM2,C,function()end)
-```
